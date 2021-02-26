37c194352c641a2a85dbf305885b24f90c1170fc
SSathish <ssathish@rapidmicrobio.com>
2021-02-26 13:54:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const defaultRoom = 'ChatApp';
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const appName = 'Chat App';
+
+app.get('/', function (req, res) {
+  // check user already present as admin
+  // if so redirect to chat directly and render past chats
+  // if not redirect to index.html go with the same flow
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Run when client connects
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(appName, 'Welcome to Chat App'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(appName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(appName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Chat Server running on port ${PORT}`));
